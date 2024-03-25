0645b94e7eb6a16d07d1d04811f450929c0c9cb3
Hengly Siev <hengly.s@pipay.com>
2024-03-26 00:39:37 +0700

    Refectoring in XCode

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,29 @@
+//
+//  LeagueVCViewController.swift
+//  app-swoosh
+//
+//  Created by BlockCode on 25/3/2567 BE.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
